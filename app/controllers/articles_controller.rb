05fe44b791913c49ab2c0f15f8631ec07a4e8991
menikdp <menik.damayanti.p@gmail.com>
2015-05-20 12:19:41 +0700

    finished

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	http_basic_authenticate_with name: "menik", password: "semarang", except: [:index, :show]
+
 	def index
 		@articles = Article.all
 	end
@@ -10,7 +12,7 @@ class ArticlesController < ApplicationController
 	def edit
 		@article = Article.find(params[:id])
 	end
-	
+
 	def create
 		@article = Article.new(article_params)
 
